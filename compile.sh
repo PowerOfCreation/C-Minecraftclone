9c83b5c9f37875921ff8d056ed9afceb1c79f7dc
PowerOfCreation <questerdesura@gmail.com>
2017-10-20 14:50:20 +0200

    Now Static Librarys

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-g++ -o main Main.cpp FastNoise.cpp shader.cpp texture.cpp -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor -lGLEW 
+g++ -o main Main.cpp FastNoise.cpp shader.cpp texture.cpp libglfw3.a libGLEW.a -Wl,-Bdynamic -lGL -lGLU -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor 
 ./main
